a2610d42bf08080d591004a4d40ebbc1020239bc
Ксения Ильина <147100614+Hioka3@users.noreply.github.com>
2024-08-05 19:44:15 +0300

    edit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const audio = document.getElementById('bg-music');
+const playImg = document.getElementById('play-img');
+
+playImg.addEventListener('click', function() {
+  if (audio.paused) {
+    audio.play();
+    playImg.src = "resourse/icons8-audio-50.png";
+  } else {
+    audio.pause();
+    playImg.src = "resourse/icons8-no-audio-50.png";
+  }
+});
