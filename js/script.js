9c5f462ceefb0c1c5d64e3e45bfa88179f271a57
Ксения Ильина <147100614+Hioka3@users.noreply.github.com>
2024-08-06 16:39:59 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const playImg = document.getElementById('play-img');
 playImg.addEventListener('click', function() {
   if (audio.paused) {
     audio.play();
-    playImg.src = "resourse/icon/icons8-audio-50.png";
+    playImg.src = "../resourse/icon/icons8-audio-50.png";
   } else {
     audio.pause();
-    playImg.src = "resourse/icon/icons8-no-audio-50.png";
+    playImg.src = "../resourse/icon/icons8-no-audio-50.png";
   }
 });
